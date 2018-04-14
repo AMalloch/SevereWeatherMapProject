af92f47263628a30e42f5da6708ec03254eb3210
Angus Malloch <angusbass@hotmail.com>
2018-04-14 08:03:21 +0100

    Realised I haden't called in app.js so added that into the HTML. Added the API and new map instance into app.js so it is loaded on event. Added CSS to create container for the map.

--- public/app.js
+++ public/app.js
@@ -1,1 +1,19 @@
+const initialize = function(){
 
+  aeris.config.set({
+    apiId:
+    apiSecret:
+  });
+
+  const main = document.getElementById('main');
+  var map = new aeris.maps.Map(main, {
+    zoom: 3,
+    center: [39.7,-93.38],
+    baseLayer: new aeris.maps.layers.AerisTile({
+      tileType: 'flat-dk',
+      zIndex: 1
+    })
+  });
+}
+
+window.addEventListener('load', initialize);
